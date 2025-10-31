67105044d3c69b5967dcbdeffdf7b836f98e2f95
Mayiocha <josemadrid09z@gmail.com>
2025-10-31 12:52:36 -0700

    CC2

--- detalles/detalles.js
+++ detalles/detalles.js
@@ -4,7 +4,22 @@ window.onload = () =>{
     for(let i=0;i<imgs.length;i++){
         imgs[i].addEventListener('click', (evt)=>{
             console.log(evt.target)
+            imgGaleria.src=evt.target.src.replace("thumbs/","")
+            imgs.forEach(item=>{
+                item.classList.remove('active')
+            })
+            evt.target.classList.add('active')
+        })
+    }
+    let btn = document.querySelector("size-btn")
+    for(let i=0;i<btn.length;i++){
+        btn[i].addEventListener('click',(evt)=>{
+            console.log(evt.target)
+            btn.forEach(item=>{
+                item.classList.remove("active")
 
+            })
+            evt.target.classList.add('active')
         })
     }
 }
